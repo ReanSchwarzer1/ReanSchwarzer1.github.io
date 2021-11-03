f4771024008f33c31057ab9e5e19b40c6199562e
Adhiraj.B <42261295+ReanSchwarzer1@users.noreply.github.com>
2021-11-03 01:16:52 -0700

    bruh

--- des157a/everypic/script.js
+++ des157a/everypic/script.js
@@ -1,8 +1,19 @@
 (function() {
     "use strict"
     console.log("reading js");
-    
 
+    /*
+    const scrollContainer = document.getElementsByClassName("main");
+    scrollContainer.addEventListener('wheel', (evt) => {
+        evt.preventDefault();
+        scrollContainer.scrollLeft += evt.deltaY;
+    });
+    */
+    var item = document.getElementsByTagName('main')[0];
+    window.addEventListener('wheel', function(event) {
+        if (event.deltaY > 0){item.scrollLeft += 100;} 
+        else {item.scrollLeft -= 100;}
+    });
 })();
 
 
