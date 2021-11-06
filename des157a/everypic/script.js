14d2fa01c86aab59af2579b55fe996b4a0102ab1
Adhiraj.B <42261295+ReanSchwarzer1@users.noreply.github.com>
2021-11-06 12:25:38 -0700

    major changes in ui and js

--- des157a/everypic/script.js
+++ des157a/everypic/script.js
@@ -9,11 +9,23 @@
         scrollContainer.scrollLeft += evt.deltaY;
     });
     */
-    var item = document.getElementsByTagName('main')[0];
-    window.addEventListener('wheel', function(event) {
-        if (event.deltaY > 0){item.scrollLeft += 100;} 
-        else {item.scrollLeft -= 100;}
+    var item = document.getElementsByTagName('main')[0]; //this is getting all elements in main 
+    window.addEventListener('wheel', function(event) { //eventlistener "listens"/waits for the user to use the mouse wheel and then begins function
+        if (event.deltaY > 0){item.scrollLeft += 100;}  //if change in deltaY (Y axis is 0), then the whole page/page in "main"  scrolls left
+        else {item.scrollLeft -= 100;} //scrolls right
     });
+
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => { //getting all <a> elements with "href" tag
+        anchor.addEventListener('click', function (e) { //listening for click (when the user clicks one of the <a> href hyperlinks)
+            e.preventDefault(); 
+    
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth' //it gets the selected attribute and the page scrolls into view wherever the user clicked with behaviour "smooth"
+            });
+        });
+    });
+
 })();
 
 
