95ecd5e2952f6f7e767fd793ddff4a7ca91519c0
Adhiraj.B <42261295+ReanSchwarzer1@users.noreply.github.com>
2021-11-21 12:40:16 -0800

    Update script.js

--- des157a/gameon/script.js
+++ des157a/gameon/script.js
@@ -10,13 +10,17 @@
     const score1 = document.getElementById("score1");
     const score2 = document.getElementById("score2");
     const actionArea = document.getElementById("actions");
-    const diceSound = new Audio("media/sword.wav");
-    const switchSound = new Audio("media/switch.wav");
-    const winSound = new Audio("media/win.wav");
-    const playerDivs = [
+
+    // variables for sound effects
+    const soundDice = new Audio("media/sword.wav");
+    const soundSwitch = new Audio("media/switch.wav");
+    const winStateSound = new Audio("media/win.wav");
+    const knightDivs = [
         document.getElementById("k1"),
         document.getElementById("k2"),
     ];
+
+    //gameobject containing the die, knights, scores and other important variables
 
     let gameData = {
         dice: [
@@ -36,18 +40,20 @@
         gameEnd: 29,
     };
 
+    //function that starts the whole game
     startGame.addEventListener("click", function () {
         gameData.index = Math.round(Math.random());
         gameControl.innerHTML = "<h2></h2>";
         gameControl.innerHTML += '<button id="quit">Quit Game?</button>';
 
-        document.getElementById("quit").addEventListener("click", function () {
+        document.getElementById("quit").addEventListener("click", function () { //function that ends the game and restarts it
             location.reload();
         });
 
         throwDice();
     });
 
+    //function for dice turns (when the turn switches between each knight)
     function setUpTurn() {
         game.innerHTML = `<h3>Roll the dice for ${gameData.players[gameData.index]}</h3>`;
         actionArea.innerHTML = '<button id="roll">Roll the Dice</button>';
@@ -56,6 +62,7 @@
         });
     }
 
+    // function with if else statements that determine the win/lose condition and display them
     function checkWinningCondition() {
         if (gameData.score[gameData.index] > gameData.gameEnd) {
             showCurrentScore();
@@ -70,15 +77,18 @@
         }
     }
 
+    //constantly updates the currentscores after each turn for both the players
     function showCurrentScore() {
         score1.innerHTML = `score: ${gameData.score[0]}`;
         score2.innerHTML = `score: ${gameData.score[1]}`;
     }
-
+    
+    //function for throwing the dice
     function throwDice() {
         actionArea.innerHTML = "";
         dice.innerHTML = "";
 
+        //dice roll - randomized
         gameData.roll1 = Math.floor(Math.random() * 6) + 1;
         gameData.roll2 = Math.floor(Math.random() * 6) + 1;
 
@@ -87,27 +97,28 @@
         gameData.rollSum = gameData.roll1 + gameData.roll2;
 
         /*
-        playerDivs[gameData.index].classList.add("sword");
+        knightDivs[gameData.index].classList.add("sword");
         setTimeout(function () {
-            playerDivs[gameData.index].classList.remove("sword");
+            knightDivs[gameData.index].classList.remove("sword");
         }, 500);
         */
         
-        playerDivs[gameData.index].classList.add("slash");
+        //adds the slash animation and then removes it after the turn finishes
+        knightDivs[gameData.index].classList.add("slash");
         setTimeout(function () {
-            playerDivs[gameData.index].classList.remove("slash");
+            knightDivs[gameData.index].classList.remove("slash");
         }, 500);
 
-        if (gameData.rollSum === 2) { 
-            switchSound.play();
+        if (gameData.rollSum === 2) { //when snake eyes
+            soundSwitch.play();
             game.innerHTML += "<h3>Oh snap! Snake eyes!</h3>";
             gameData.score[gameData.index] = 0;
             gameData.index ? (gameData.index = 0) : (gameData.index = 1); 
             checkWinningCondition();
 
-            setTimeout(setUpTurn, 2000);
+            setTimeout(setUpTurn, 2000); //delay of 2 seconds for switching
         } else if (gameData.roll1 === 1 || gameData.roll2 === 1) { 
-            switchSound.play();
+            soundSwitch.play();
             gameData.index ? (gameData.index = 0) : (gameData.index = 1); 
             game.innerHTML += `<h3>Sorry one of your rolls was a one, switching to ${gameData.players[gameData.index]}</h3>`;
 
@@ -118,25 +129,22 @@
             actionArea.innerHTML =
                 '<button id="rollagain">Roll Again</button> or <button id="pass">Pass</button>';
 
-            document
-                .getElementById("rollagain")
-                .addEventListener("click", function () {
+            document.getElementById("rollagain").addEventListener("click", function () {
                     throwDice();
                 });
 
-            document
-                .getElementById("pass")
-                .addEventListener("click", function () {
+            document.getElementById("pass").addEventListener("click", function () {
                     gameData.index
                         ? (gameData.index = 0)
                         : (gameData.index = 1);
                     setUpTurn();
                 });
 
+            // checks to see if any of the knights won
             if (checkWinningCondition()) {
-                winSound.play();
+                winStateSound.play();
             } else {
-                diceSound.play();
+                soundDice.play();
             }
         }
 
